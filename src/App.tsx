5857c274c46cc6c5517823aa2fba479ffdb10712
Taha <tsid2181@gmail.com>
2023-09-04 19:55:26 -0400

    added Vans page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
 import '../server.js'
 
 
@@ -11,11 +12,13 @@ function App() {
         <Link to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
