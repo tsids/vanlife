3a5f31c19522265e6a13881026368a9b29359cb0
Taha <tsid2181@gmail.com>
2023-09-04 21:17:27 -0400

    added more routes/pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Home from "./pages/Vans/Home"
+import About from "./pages/Vans/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from './pages/Host/Dashboard'
 import Layout from "./components/Layout"
-import '../server.js'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 
+import '../server.js'
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/vans' element={<Vans />} />
           <Route path='/vans/:id' element={<VanDetail />} />
+          <Route path='/host' element={<Dashboard />} />
+          <Route path='/host/income' element={<Income />} />
+          <Route path='/host/reviews' element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
