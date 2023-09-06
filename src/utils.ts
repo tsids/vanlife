0630145fd2161d99dcec99d4a40dfea04586286c
Taha <tsid2181@gmail.com>
2023-09-06 18:00:57 -0400

    changed function way

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { redirect } from "react-router-dom"
 
-export const requireAuth = async () => {
+export async function requireAuth() {
     const isLoggedIn = false
     if (!isLoggedIn) {
         const res: any = redirect("/login")
